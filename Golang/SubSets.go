0e5e46bf9a9bed2cab99e452ec12b1264aaf87cb
Rustam <rustamutkirov3141@gmail.com>
2024-05-28 16:49:06 +0200

    Subsets has been solved

--- Golang/SubSets.go
+++ Golang/SubSets.go
@@ -1,22 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
+var allSubsets [][]int 
 
-func subsets(nums [][]int, n int) {
-	size := len(nums) - 1
+func AllSubsets(nums [][]int, n int) {
+	size := len(nums[0])
 	if n == size {
-		for i := 0; i <= size; i++ {
+		var sets []int
+		for i := 0; i < size; i++ {
 			if nums[1][i] == 0 {
-				fmt.Printf("%d ", nums[0][i])		
+				sets = append(sets, nums[0][i])		
 			}
 		}
-		fmt.Printf("\n")
+		allSubsets = append(allSubsets, sets)
 		return
 	}
 	nums[1][n] = 0;
-	subsets(nums, n + 1)
+	AllSubsets(nums, n + 1)
 	nums[1][n] = 1
-	subsets(nums, n + 1)
+	AllSubsets(nums, n + 1)
+}
+
+func subsets(nums []int) [][]int {
+	_nums := make([][]int, 2)
+	for i := 0; i < 2; i++ {
+		_nums[i] = make([]int, len(nums))
+	}
+	_nums[0] = nums
+	AllSubsets(_nums, 0)
+	return allSubsets
 }
